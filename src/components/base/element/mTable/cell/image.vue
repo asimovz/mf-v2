08be9b2fc161da9f2c18c6b5f22334da99dba2ab
管晓伟 <405171253@qq.com>
2020-06-18 11:36:17 +0800

    6月17日，UI文档修正（部分）

--- src/components/base/element/mTable/cell/image.vue
+++ src/components/base/element/mTable/cell/image.vue
@@ -15,77 +15,90 @@
 </template>
 <script>
 export default {
-  name: "ag-image",
+  name: 'ag-image',
   props: {
     params: {
       type: Object,
       default: () => ({})
     }
   },
-  data() {
+  data () {
     return {
-      id: "",
+      id: '',
       hover: false,
-      imgUrl: "",
-      alt: "",
-      width: "",
+      imgUrl: '',
+      alt: '',
+      width: '',
       height: 28,
-      style: "",
+      style: '',
       imgDiv: null
-    };
+    }
   },
-  beforeMount() {
-    this.id = this.params.props.id;
-    this.hover = this.params.props.hover;
-    this.alt = this.params.props.alt;
-    this.width = this.params.props.width;
-    this.height = this.params.props.height;
-    this.style = this.params.props.style;
-    if (this.params.props.urlType === "plain") {
-      this.imgUrl = this.params.value;
+  beforeMount () {
+    this.id = this.params.props.id
+    this.hover = this.params.props.hover
+    this.alt = this.params.props.alt
+    this.width = this.params.props.width
+    this.height = this.params.props.height
+    this.style = this.params.props.style
+    if (this.params.props.urlType === 'plain') {
+      this.imgUrl = this.params.value
     } else {
       this.imgUrl =
         this.params.props.transitionUrl +
-        "?" +
+        '?' +
         this.params.colName +
-        "=" +
-        this.params.value;
+        '=' +
+        this.params.value
     }
   },
   methods: {
-    show(evt) {
-      if (!this.hover) return;
-      //原始宽高
-      let imgHeight = this.$refs.img.naturalHeight;
-      let imgWidth = this.$refs.img.naturalWidth;
-      //屏幕宽高
-      let bodyRect = document.body.getBoundingClientRect();
-      let bodyHeight = bodyRect.height;
-      let bodyWidth = bodyRect.width;
-      let scrollHeight = document.documentElement.scrollTop;
+    show (evt) {
+      if (!this.hover) return
+      // 原始宽高
+      let imgHeight = this.$refs.img.naturalHeight
+      let imgWidth = this.$refs.img.naturalWidth
+      // 屏幕宽高
+      let bodyRect = document.body.getBoundingClientRect()
+      let bodyHeight = bodyRect.height
+      let bodyWidth = bodyRect.width
+      let scrollHeight = document.documentElement.scrollTop
 
-      let el = evt.target;
-      let pos = el.getBoundingClientRect();
-      let parentWidth = el.parentNode.getBoundingClientRect().width;
+      let el = evt.target
+      let pos = el.getBoundingClientRect()
+      let parentWidth = el.parentNode.getBoundingClientRect().width
 
-      let offsetX = parentWidth > pos.width ? pos.width + 10 : parentWidth;
-      offsetX = offsetX + pos.x;
-      let offsetY = pos.top + scrollHeight;
+      let offsetX = parentWidth > pos.width ? pos.width + 10 : parentWidth
+      offsetX = offsetX + pos.x
+      let offsetY = pos.top + scrollHeight
       if (offsetX + imgWidth > bodyWidth) {
-        offsetX = pos.x - imgWidth - 10;
+        offsetX = pos.x - imgWidth - 10
       }
       if (offsetY + imgHeight > bodyHeight) {
-        offsetY = offsetY - imgHeight + this.height;
+        offsetY = offsetY - imgHeight + this.height
       }
 
-      this.imgDiv = document.createElement("div");
-      this.imgDiv.style.cssText = `position: absolute;left:${offsetX}px;top:${offsetY}px;z-index: 99;`;
-      this.imgDiv.innerHTML = `<img src='${this.imgUrl}' />`;
-      document.body.appendChild(this.imgDiv);
+      this.imgDiv = document.createElement('div')
+      this.imgDiv.className = 'table-preview-img'
+      this.imgDiv.style.cssText = `position: absolute;left:${offsetX}px;top:${offsetY}px;z-index: 99;`
+      this.imgDiv.innerHTML = `<img src='${this.imgUrl}' />`
+      document.body.appendChild(this.imgDiv)
     },
-    hide() {
-      this.hover && document.body.removeChild(this.imgDiv);
+    hide () {
+      this.hover && document.body.removeChild(this.imgDiv)
     }
   }
-};
+}
 </script>
+<style lang="scss">
+.table-preview-img{
+  background-color: #fff;
+  border-radius: 8px;
+  padding:5px;
+  box-shadow: 0px 2px 4px 0px rgba(0, 0, 0, 0.12);
+  img{
+    border-radius: 8px;
+    vertical-align: bottom;
+  }
+}
+</style>
