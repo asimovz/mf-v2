a8099c50cec66fd0ebde0bb10b953a7eb748b222
常雨非 <changyufei@mfexcel.com>
2020-07-07 16:28:27 +0800

    开发“5G消息固定按钮”组件

--- src/components/business/index.js
+++ src/components/business/index.js
@@ -4,9 +4,10 @@ import brandEditor from './brandEditor' //公众号品牌信息编辑
 import brandListEditor from './brandListEditor' //公众号菜单列表编辑
 import sceneConfig from './sceneConfig' //场景方案编辑组件
 import msgConfig from './msgConfig' //卡片方案编辑组件
+import mAddShortcutButton from './mAddShortcutButton' //固定快捷按钮组件
 
 const component = [
-	motionOptions, innerMenu, menuPreview, brandEditor, brandListEditor, sceneConfig, msgConfig
+	motionOptions, innerMenu, menuPreview, brandEditor, brandListEditor, sceneConfig, msgConfig, mAddShortcutButton
 ]
 
 export default component
---
+++ src/components/business/mAddShortcutButton/childContent.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="childContent">
+    <div class="defaultContent">
+      <m-select :options="btnList" @on-change="handleChange" v-model="btn.type" size="small" class="childSelect"></m-select>
+      <m-input class="childInput" v-model="btn.description" size="small" placeholder="按钮文案" />
+      <i class="el-icon-delete" @click="deleteBtn"></i>
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='url'">
+      <m-input class="matchInput" v-model="btn.url" size="small" placeholder="链接需以http(s)://开头" />
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='openApp'">
+      <m-input class="matchInput" v-model="btn.openAppAndroid" size="small" placeholder="[Android]请输入正确调起地址" />
+      <m-input class="matchInput" v-model="btn.openAppIos" size="small" placeholder="[ios]请输入正确调起地址" />
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='call'">
+      <m-input class="matchInput" v-model="btn.phoneNum" size="small" placeholder="请输入电话号码" />
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='addressLocation'">
+      <m-input class="matchInput" v-model="btn.sendAddressPlaceName" size="small" placeholder="地点名称" />
+      <m-input class="matchInput" v-model="btn.sendAddressLongitude" size="small" placeholder="经度" />
+      <m-input class="matchInput" v-model="btn.sendAddressLatitude" size="small" placeholder="纬度" />
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='screen'">
+      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" />
+    </div>
+
+    <div class="btnMatch" v-if="btn.type=='bringUp'">
+      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" />
+      <m-input class="matchInput" v-model="btn.presendContent" size="small" placeholder="预发送内容" />
+    </div>
+    <input type="hidden" name="addshortcutbutton" :value="data"/>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    data:{},
+    index: {
+      type: Number,
+      required: true
+    },
+    items: {
+      type: Array,
+      default: Array
+    }
+  },
+  data(){
+    return {
+      btnList: [
+        {
+          value: 'reply',
+          label: '回复按钮'
+        },
+        {
+          value: 'url',
+          label: '跳转按钮'
+        },
+        {
+          value: 'openApp',
+          label: '打开app按钮'
+        },
+        {
+          value: 'call',
+          label: '打电话按钮'
+        },
+        {
+          value: 'sendAddress',
+          label: '发送地址按钮'
+        },
+        {
+          value: 'addressLocation',
+          label: '地址定位按钮'
+        },
+        {
+          value: 'screen',
+          label: '拍摄按钮'
+        },
+        {
+          value: 'bringUp',
+          label: '调起指定联系人'
+        },
+      ],
+      btn:{
+        type:'reply',
+        description:'',
+        url:'',
+        openAppAndroid:'',
+        openAppIos:'',
+        phoneNum:'',
+        sendAddressPlaceName:'',
+        sendAddressLongitude:'',
+        sendAddressLatitude:'',
+        targetContact:'',
+        presendContent:''
+      }
+    }
+  },
+  created(){
+    this.btn = this.data
+    console.log('最里面6:', JSON.stringify(this.data))
+    console.log('最里面66:', JSON.stringify(this.items))
+  },
+  computed: {
+
+  },
+  watch: {
+    btn: {
+      handler (newV, oldV) {
+        if (newV.description.length === 0) {
+          return false
+        }
+
+        this.$emit('uploadData', {index: this.index, data: newV})
+      },
+      deep: true
+    },
+    items: {
+      handler (newV, oldV) {
+        if (newV.length !== 0) {
+          this.student = {...newV[this.index]}
+        }
+      },
+      deep: true
+    }
+  },
+  methods: {
+    handleChange(){
+      Object.keys(this.btn).forEach(key => {
+        if(key!='type'){
+          (this.btn)[key] = ''
+        }
+      })
+    },
+    deleteBtn(){
+      this.$emit('deleteIndex', this.index)
+    }
+  }
+}
+</script>
+<style lang='less' scoped>
+  .childContent{
+    width: 100%;
+    min-height: 65px;
+    padding: 16px;
+    box-sizing: border-box;
+  }
+  .defaultContent{
+    display: flex;
+    flex-direction: row;
+  }
+  .childInput{
+    margin-left: 10px;
+    width: 300px !important;
+  }
+  .el-icon-delete{
+    margin: 8px 0 0 0;
+    cursor: pointer;
+  }
+</style>
