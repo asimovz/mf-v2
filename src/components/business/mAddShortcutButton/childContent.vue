fed582ed6db742ee89404ae2b57dcb14ada6be3d
常雨非 <changyufei@mfexcel.com>
2020-07-08 18:10:10 +0800

    输入框验证，字段限制，倒计数显示

--- src/components/business/mAddShortcutButton/childContent.vue
+++ src/components/business/mAddShortcutButton/childContent.vue
@@ -2,36 +2,35 @@
   <div class="childContent">
     <div class="defaultContent">
       <m-select :options="btnList" @on-change="handleChange" v-model="btn.type" size="small" class="childSelect"></m-select>
-      <m-input class="childInput" v-model="btn.description" size="small" placeholder="按钮文案" />
+      <m-input class="childInput text-required" v-model="btn.description" size="small" placeholder="按钮文案" :maxlength="10" :validate="'required'" validate-msg="请输入按钮文案" />
       <i class="el-icon-delete" @click="deleteBtn"></i>
     </div>
 
     <div class="btnMatch" v-if="btn.type=='url'">
-      <m-input class="matchInput" v-model="btn.url" size="small" placeholder="链接需以http(s)://开头" />
+      <m-input class="matchInput" v-model="btn.url" size="small" placeholder="链接需以http(s)://开头" :validate="{regex:/^(http[s]{0,1}:\/\/)/i}" validate-msg="请输入正确链接" />
     </div>
 
     <div class="btnMatch" v-if="btn.type=='openApp'">
-      <m-input class="matchInput" v-model="btn.openAppAndroid" size="small" placeholder="[Android]请输入正确调起地址" />
-      <m-input class="matchInput" v-model="btn.openAppIos" size="small" placeholder="[ios]请输入正确调起地址" />
+      <m-input class="matchInput" v-model="btn.openAppAndroid" size="small" placeholder="[Android]请输入正确调起地址" :validate="'required'" validate-msg="请输入正确调起地址" />
     </div>
 
     <div class="btnMatch" v-if="btn.type=='call'">
-      <m-input class="matchInput" v-model="btn.phoneNum" size="small" placeholder="请输入电话号码" />
+      <m-input class="matchInput" v-model="btn.phoneNum" size="small" placeholder="请输入电话号码" :validate="{regex:/^[\d\-\s\+]+$/}" validate-msg="请输入正确电话号码" />
     </div>
 
     <div class="btnMatch" v-if="btn.type=='addressLocation'">
-      <m-input class="matchInput" v-model="btn.sendAddressPlaceName" size="small" placeholder="地点名称" />
-      <m-input class="matchInput" v-model="btn.sendAddressLongitude" size="small" placeholder="经度" />
-      <m-input class="matchInput" v-model="btn.sendAddressLatitude" size="small" placeholder="纬度" />
+      <m-input class="matchInput" data-name="longInput" v-model="btn.sendAddressPlaceName" size="small" placeholder="位置名称" :maxlength="20" :validate="'required'" validate-msg="请输入位置名称" />
+      <m-input class="matchInput" v-model="btn.sendAddressLongitude" size="small" placeholder="经度" :validate="'required'" validate-msg="请输入经度" />
+      <m-input class="matchInput" v-model="btn.sendAddressLatitude" size="small" placeholder="纬度" :validate="'required'" validate-msg="请输入纬度" />
     </div>
 
     <div class="btnMatch" v-if="btn.type=='screen'">
-      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" />
+      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" :validate="'required'" validate-msg="请输入目标联系人" />
     </div>
 
     <div class="btnMatch" v-if="btn.type=='bringUp'">
-      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" />
-      <m-input class="matchInput" v-model="btn.presendContent" size="small" placeholder="预发送内容" />
+      <m-input class="matchInput" v-model="btn.targetContact" size="small" placeholder="目标联系人" :validate="'required'" validate-msg="请输入目标联系人" />
+      <m-input class="matchInput" data-name="longInput" v-model="btn.presendContent" size="small" placeholder="预发送内容" :maxlength="100" />
     </div>
     <input type="hidden" name="addshortcutbutton" :value="data"/>
   </div>
@@ -51,6 +50,7 @@ export default {
   },
   data(){
     return {
+      required:true,
       btnList: [
         {
           value: 'reply',
@@ -102,8 +102,6 @@ export default {
   },
   created(){
     this.btn = this.data
-    console.log('最里面6:', JSON.stringify(this.data))
-    console.log('最里面66:', JSON.stringify(this.items))
   },
   computed: {
 
@@ -111,10 +109,6 @@ export default {
   watch: {
     btn: {
       handler (newV, oldV) {
-        if (newV.description.length === 0) {
-          return false
-        }
-
         this.$emit('uploadData', {index: this.index, data: newV})
       },
       deep: true
@@ -122,7 +116,7 @@ export default {
     items: {
       handler (newV, oldV) {
         if (newV.length !== 0) {
-          this.student = {...newV[this.index]}
+          this.btn = {...newV[this.index]}
         }
       },
       deep: true
--- src/components/business/mAddShortcutButton/readme.md
+++ src/components/business/mAddShortcutButton/readme.md
@@ -1,6 +1,7 @@
 ## 模板使用说明 ##
 
-快捷按钮和设置中的固定按钮总和不能超过10个
+1、快捷按钮和设置中的固定按钮总和不能超过10个
+2、删除时不显示弹窗
 
 在HTML中引用：
 <add-shortcutbutton :data="addBTNData" />
@@ -10,19 +11,50 @@ addBTNData = []
 
 1、“回复按钮”
 {"type":"reply","description":""}
+校验：
+description：10个字，必填
+
 2、“跳转按钮”
 {"type":"url","description":"","url":""}
+校验：
+description：10个字，必填
+url：以http/https开头，必填
+
 3、“打开app按钮”
-{"type":"openApp","description":"","url":"","openAppAndroid":"","openAppIos":""}
+{"type":"openApp","description":"","openAppAndroid":""}
+校验：
+description：10个字，必填
+openAppAndroid： 必填
+
 4、“打电话按钮”
-{"type":"call","description":"","url":"","phoneNum":""}
+{"type":"call","description":"","phoneNum":""}
+校验：
+description：10个字，必填
+phoneNum： 号码校验（只能输入数字，+，-，空格），必填
+
 5、“发送地址按钮”
 {"type":"sendAddress","description":""}
+校验：
+description：10个字，必填
+
 6、“地址定位按钮”
-{{"type":"addressLocation","description":"","sendAddressPlaceName":"","sendAddressLongitude":"","sendAddressLatitude":""}}
+{"type":"addressLocation","description":"","sendAddressPlaceName":"","sendAddressLongitude":"","sendAddressLatitude":""}
+校验：
+description：10个字，必填
+sendAddressPlaceName：20个字，必填
+sendAddressLongitude：必填
+sendAddressLatitude：必填
+
 7、“拍摄按钮”
 {"type":"screen","description":"","targetContact":""}
+校验：
+description：10个字，必填
+targetContact：必填
+
 8、“调起指定联系人”
 {"type":"bringUp","description":"","targetContact":"","presendContent":""}
-
+校验：
+description：10个字，必填
+targetContact：必填
+presendContent：100个字
 
