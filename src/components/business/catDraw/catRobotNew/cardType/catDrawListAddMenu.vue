a9ee51d7a8ef4d8c57a75ca7a688ababefafc783
yuyang <yuyang@mfexcel.com>
2020-07-08 09:13:13 +0800

    5g信息编辑组件

---
+++ src/components/business/catDraw/catRobotNew/cardType/catDrawListAddMenu.vue
@@ -0,0 +1,182 @@
+<template>
+	<div :style="listStyle">
+		<div class="flex-center add-content" style="color:rgba(0,0,0,1); align-items: flex-start;">
+			<template v-if="!showMaxMenus">
+				<div class="cat-draw-title">
+					添加内容
+				</div>
+				<div class="flex-center " style="flex-wrap: wrap; justify-content: flex-start;  align-items: flex-start;">
+					<!--<div v-for="(item, index) in menus" :key="index" @click="addMenu" :data-type="item.type" class="flex-center add-content-txt">
+						<img class="add-content-img" :src="item.img" /> {{item.txt}}
+					</div>-->
+					<template v-for="(item, index) in menus" >
+						<div v-if="!selectOnlyOne"  @click="addMenu"  :data-type="item.type" class="flex-center add-content-txt add-content-txt1">
+							<img class="add-content-img" :src="item.img" /> {{item.txt}}
+						</div>
+						<div v-else :class="{'add-content-txt1' : selectOnlyOneObj.replyType == item.type}" @click="addMenu"  :data-type="item.type" class="flex-center add-content-txt">
+							<img class="add-content-img" :src="selectOnlyOneObj.replyType == item.type ? item.img : item.img1" /> {{item.txt}}
+						</div>
+					</template>
+				</div>
+			</template>
+			
+			<!--已达上限-->
+			<!--<div v-else class="content-limit-reached">内容数量已达上限</div>-->
+			<div v-else class="flex-center add-button"  style="color:#A9AAB1">
+					<Icon type="ios-plus-empty" size="20" style="margin-right: 6px;"></Icon>
+					内容数量已达上限
+				</div>
+		</div>
+		
+	</div>
+</template>
+<script>
+	export default {
+		name: 'catDrawListAddMenu', //图片，音频和视频
+		props: {
+			listStyle: {
+				width: '351px',
+			},
+			showMaxMenus: {
+				type: Boolean,
+				default: false, //是否显示上限按钮
+			},
+			selectOnlyOne: {
+				type: Boolean,
+				default: false, //0527新增是否值选择一个，默认是可以选择多个的
+			},
+			//0527新增是否值选择一个，默认是可以选择多个的
+			selectOnlyOneObj: {
+				type: Object,
+				default: function() {
+					return {
+						nodeType: 'text', 
+					}
+				},
+			},
+		},
+		components: {},
+		data: function() {
+			return {
+				menus: [{
+						txt: '文字',
+						type: 'text',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/text.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/text1.png', //未选中的状态
+					},
+					{
+						txt: '图片',
+						type: 'image',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/images.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/images1.png', //未选中的状态
+					},
+					{
+						txt: '音频',
+						type: 'voice',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/audio.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/audio1.png', //未选中的状态
+					},
+					{
+						txt: '视频',
+						type: 'video',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/video.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/video1.png', //未选中的状态
+					},
+					{
+						txt: '单卡',
+						type: 'singleCard',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/simpleCard.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/simpleCard1.png', //未选中的状态
+					},
+					{
+						txt: '多卡',
+						type: 'manyCards',
+						img: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/multipleCard.png',
+						img1: 'http://down3.bizport.cn/rcs/chatbot/images/20200318/multipleCard1.png', //未选中的状态
+					},
+				]
+			}
+		},
+		methods: {
+			addMenu(e) {
+				var that = this
+				var dataset = e.currentTarget.dataset
+				var clickType = dataset.type
+				this.$emit("addMenu", {
+					addMenuType: clickType
+				}); //自定义事件 此方法直接出发父组件中的显示数据的方法
+			},
+		},
+		created() {},
+		mounted() {
+		},
+		destroyed() {
+			document.body.style.overflow = 'auto' //处理弹窗导致无法关闭的bug
+		},
+	}
+</script>
+<style lang='less' scoped>
+	@import '../../asset/css/default.css';
+	@default: #8C8C8C;
+	@defaultBorder: #EBEEF5;
+	.add-content {
+		font-size: 14px;
+		line-height: 22px;
+		cursor: pointer;
+		height: inherit;
+		justify-content: flex-start;
+		/*padding: 17px 16px;*/
+		padding: 12px 16px;
+	}
+	
+	.add-content-icon {
+		margin-right: 6px;
+	}
+	
+	.add-content-txt {
+		font-size: 14px;
+		/*color: rgba(24, 144, 255, 1);*/
+		margin-left: 15px;
+		/*移上去显示手指*/
+		cursor: pointer;
+		
+		color: #979797;
+	}
+	
+	.add-content-txt1 {
+		color: rgba(24, 144, 255, 1);
+	}
+	
+	.add-content-img {
+		width: 12px;
+		height: 12px;
+		margin-right: 6px;
+	}
+	
+	.instance-top-content {
+		width: 100%;
+		height: 198px;
+		background: rgba(255, 255, 255, 1);
+	}
+	
+	.upload-img {
+		width: 116px;
+		height: 116px;
+	}
+	
+	.cat-draw-title {
+		min-width: 60px;
+	}
+	
+	/*新增回复已达上限*/
+	.content-limit-reached {
+		font-size:14px;
+		color:rgba(0,0,0,0.25);
+	}
+	.add-button {
+		font-size: 12px;
+		border-radius: 0 0 4px 4px;
+		padding: 0 16px;
+		cursor: pointer;
+	}
+</style>
---
+++ src/components/business/catDraw/roInputAboutQuoteOne/roInputAboutQuoteOne.vue
@@ -0,0 +1,243 @@
+<template>
+	<div class="flex-center" style="position: relative;" :style="inputQuoteStyle">
+			<div class="txt-limit-div" :class="{'handle-text-blocking' : maxlength }" style="position: relative; width: 100%;">
+  					<!-- <Input v-model="inputAboutQuoteOne[quoteKey]" @on-change="onChangeInput($event)" :type="type" :rows="1" :placeholder="placeholder" :id="inputId" :size="size" /> -->
+  					<m-input :value="inputAboutQuoteOne[quoteKey]" :maxlength="maxlength" @input="onChangeInput" :type="type" :rows="1" :placeholder="placeholder" :id="inputId" :size="size" />
+  					<!-- <template v-if="maxlength">
+  						<span v-if="size=='small'" class="txt-limit1" >{{calculateStringActualLength(inputAboutQuoteOne[quoteKey])}}/{{maxlength}}</span>
+
+  						<span v-else class="txt-limit">{{calculateStringActualLength(inputAboutQuoteOne[quoteKey])}}/{{maxlength}}</span>
+  					</template> -->
+  				</div>
+
+			<!--新增emoji输入-->
+			<roEmoji  v-if="showQuoteEmoji"  @addCatEmoji="addCatEmoji" class="about-quote-icon" :currentIndex="index" :nodeList="nodeList" :otherNodeList="otherNodeList" :nowNode="nowNode" ></roEmoji>
+
+			<catDrawPoptip v-if="showQuote"  class="about-quote-icon" @drawQuote="drawQuote" :currentIndex="index" :nodeList="nodeList" :otherNodeList="otherNodeList" :nowNode="nowNode" :size="size"></catDrawPoptip>
+		</div>
+</template>
+<script>
+	import catDrawPoptip from '../catDrawPoptip' //引用的图标
+
+	import roEmoji from '../roEmoji/roEmoji'  //emoji输入框
+
+	import { removeEmoji, getUUID, emojiCharStringLen, emojiCharStringSubstr } from '../util'
+
+	//带上引用和emoji输入的input输入框
+	export default {
+		name: 'roInputAboutQuoteOne',
+		components: {
+			catDrawPoptip,
+			roEmoji,
+		},
+		props: {
+			nowNode: Object, //当前的节点
+			nodeList: Array, //所有的节点
+			otherNodeList: Array, //合并其他任务的节点
+			//主要看下面3个值是否需要满足
+			inputAboutQuoteOne: {
+				type: Object,
+				default: function() {
+					return {}
+				},
+			}, //循环的引用内容
+
+			maxlength: {
+				type: Number,
+				default: undefined,
+			}, //长度限制，用于截取数据
+			//设置样式
+			inputQuoteStyle: {
+				type: Object,
+				default: function() {
+					return {}
+				},
+			},
+			//input的type，可以是text或textarea
+			type: {
+				type: String,
+				default: 'text',
+			},
+			quoteKey: {
+				type: String,
+				default: 'originalMessage',
+			},
+			size: {
+				type: String,
+				default: 'default', //设置input时的small
+			},
+			placeholder: {
+				type: String,
+				default: '请输入', //设置默认的提示
+			},
+
+			showQuote: {
+				type: Boolean,
+				default: false, //是否显示引用组件
+			},
+			showQuoteEmoji: {
+				type: Boolean,
+				default: false, //是否显示emoji引用输入
+			},
+		},
+		data: function() {
+			return {
+				inputId: getUUID(),
+				index: 0,
+			}
+		},
+		computed:{
+		  //改变备注的长度，长度大于14位就用...代替剩余内容
+		  calculateStringActualLength(){
+		      //text就是所传参数
+		      return function (text) {
+		      		var len = 0
+		      		if(text) {
+		      			len = emojiCharStringLen(text)
+		      		}
+		    			return len
+		      }
+		  }
+		},
+		methods: {
+			drawQuote(data) {
+				this.selectionStart(data.currentIndex, data.nodeName) //添加到原来的input里面去
+			},
+			selectionStart(index, quote) {
+				//				quote = `{${quote}}`
+				//添加到原来的内容里
+				//				var index = dataset.index
+
+//				var emojiInput = this.inputId + index //获取的id
+				var emojiInput = this.inputId //获取的id
+				var elInput = document.getElementById(emojiInput); //根据id选择器选中对象
+				var startPos = elInput.selectionStart; // input 第0个字符到选中的字符
+				var endPos = elInput.selectionEnd; // 选中的字符到最后的字符
+				if(startPos === undefined || endPos === undefined) return
+				var txt = elInput.value;
+				//		      // 将表情添加到选中的光标位置
+				var result = txt.substring(0, startPos) + quote + txt.substring(endPos)
+
+				//	如果有才去限制长度
+				if(this.maxlength) {
+//					result = result.slice(0, this.maxlength)
+					result = emojiCharStringSubstr(result, 0, this.maxlength) //截取实际的长度
+				}
+
+				elInput.value = result; // 赋值给input的value
+				// 重新定义光标位置
+				elInput.focus();
+				elInput.selectionStart = startPos + quote.length;
+				elInput.selectionEnd = startPos + quote.length;
+
+				//处理没有触发input的值
+				const inputEvent = new Event('input')
+				elInput.dispatchEvent(inputEvent)
+			},
+
+//			//新增监听input改变值
+			onChangeInput(val) {
+				//如果没有，不用去设置了
+				if(!this.maxlength) {
+					return
+				}
+
+				// var value = event.target.value
+				var value = val
+
+				var len = emojiCharStringLen(value)
+				//需要取截取长度
+				if(len <= this.maxlength) {
+					return
+				}
+
+				var result = emojiCharStringSubstr(value, 0, this.maxlength) //截取实际的长度
+				var emojiInput = this.inputId //获取的id
+				var elInput = document.getElementById(emojiInput); //根据id选择器选中对象
+				elInput.value = result; // 赋值给input的value
+
+				//处理没有触发input的值
+				const inputEvent = new Event('input')
+				elInput.dispatchEvent(inputEvent)
+			},
+			//新增添加emoji表情
+			addCatEmoji(data) {
+				this.selectionStart(data.currentIndex, data.emoji) //添加到原来的input里面去
+			},
+		},
+		created() {
+			var that = this
+		},
+		mounted() {},
+		directives: {
+			dbClick: {
+				inserted(el, binding) {
+					el.addEventListener('click', e => {
+						if(!el.disabled) {
+							el.disabled = true;
+							let timer = setTimeout(() => {
+								el.disabled = false;
+							}, 1000)
+						}
+					})
+				}
+			}
+		},
+	}
+</script>
+
+<style lang='less' scoped>
+	@import '../asset/css/default.css';
+	/*新增文字长度提示限制*/
+	.txt-limit-div {
+		/deep/ .ivu-input {
+			resize: none;
+		}
+	}
+	/*0508新增挡住文字的问题,如果有文字长度限制就加上这个*/
+	.handle-text-blocking {
+		/deep/ .ivu-input {
+			padding-right: 43px !important;
+		}
+	}
+
+	.txt-limit {
+		position: absolute;
+		top: 2px;
+		right: 5px;
+		background: #fff;
+		height: 30px;
+		line-height: 30px;
+		border-radius: 4px;
+		opacity: 0.25;
+	}
+
+	.txt-limit1 {
+		position: absolute;
+		top: 1px;
+		right: 5px;
+		background: #fff;
+		height: 22px;
+		line-height: 22px;
+		color: #000;
+		border-radius: 4px;
+		opacity: 0.25;
+	}
+
+	.about-quote-one {
+		position: absolute;
+		right: 1px;
+		top: 5px;
+	}
+	/*小输入框时的样式*/
+
+	.about-quote-one-small {
+		position: absolute;
+		right: 2px;
+		top: 2px;
+	}
+
+	.about-quote-icon {
+		margin-left: 15px;
+	}
+</style>
