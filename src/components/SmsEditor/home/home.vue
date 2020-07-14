9b3e7ec9bbe928a0dc727c222f9ce1038ead354c
huanghe <huanghe@mfexcel.com>
2020-07-14 16:30:11 +0800

    返回提示文案修改

--- src/components/SmsEditor/home/home.vue
+++ src/components/SmsEditor/home/home.vue
@@ -279,7 +279,7 @@ export default {
       })
     },
     goBack() {
-      this.$confirm('即将丢失所有修改，确认返回?', '提示', {
+      this.$confirm('未保存修改将丢失，确认返回?', '提示', {
         type: 'warning'
       }).then(res => {
         window.history.back()
