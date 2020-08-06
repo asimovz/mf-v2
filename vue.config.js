e057a9eb92514ef44cd9ebb291d39a540a63c0c1
asimovz <asimovz@gmail.com>
2020-08-06 09:44:39 +0800

    样式调整

--- vue.config.js
+++ vue.config.js
@@ -116,9 +116,8 @@ module.exports = {
     config.resolve.alias
       .set("vue$", "vue/dist/vue.esm.js")
       .set("@", resolve("src"))
-      .set("static", resolve("static"))
+      .set("static", path.resolve(__dirname, './public/static'))
       .set("assets", path.resolve(__dirname, './src/assets'))
-      .set("utils", path.resolve(__dirname, './src/components/baseComponent/iview/utils'))
       .set("node_modules", path.resolve(__dirname, './node_modules'))
       .set("accredit",process.env.NODE_ENV === 'production' ? path.resolve(__dirname, './src/assets/js/accredit.js'):path.resolve(__dirname, './src/assets/js/accredit_dev.js'));
 
